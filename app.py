d996ebb37f8bf420a9014183571c3c92ffe851dd
quoth-the-dev <andrewgorman.dev@gmail.com>
2023-10-11 23:28:04 +0200

    Start binomial

--- app.py
+++ app.py
@@ -19,14 +19,14 @@ def mean():
 
 @app.route("/mean-form", methods=["POST"])
 def mean_form():
-    data_list, dp, error = _process_form_data_as_list()
+    data_list, dp, error = _discreet_data_from_form()
     if error is not None:
         return render_template("error.html", data=error[0], error=error[1])
     measures = processor.get_central_tendency(data_list=data_list, dp=dp)
     return render_template("mean-form.html", measures=measures, data=data_list)
 
 
-def _process_form_data_as_list() -> Tuple[List[float], int, Optional[Tuple[str, str]]]:
+def _discreet_data_from_form() -> Tuple[List[float], int, Optional[Tuple[str, str]]]:
     error = None
     data_list = []
     accuracy = request.form.get("accuracy", "2dp")
@@ -41,14 +41,12 @@ def _process_form_data_as_list() -> Tuple[List[float], int, Optional[Tuple[str, 
     try:
         data_list = list(map(lambda x: float(x), data.split(",")))
     except ValueError:
-        data = (data,)
         error = (
             data,
             "Please only enter numbers using '.' for decimal point. "
             "It could be an unwanted character or maybe you used ',' "
             "as a decimal marker?",
         )
-
     return data_list, dp, error
 
 
@@ -59,7 +57,7 @@ def spread():
 
 @app.route("/spread-form", methods=["POST"])
 def spread_form():
-    data_list, dp, error = _process_form_data_as_list()
+    data_list, dp, error = _discreet_data_from_form()
     if error is not None:
         return render_template("error.html", data=error[0], error=error[1])
     measures = processor.get_spread(data_list=data_list, dp=dp)
@@ -69,6 +67,23 @@ def spread_form():
 @app.route("/binomial")
 def binomial():
     return render_template("binomial.html", title="Binomial")
+
+
+@app.route("/binomial-form", methods=["POST"])
+def binomial_form():
+    # accuracy = request.form.get("accuracy", "2dp")
+
+    distribution = request.form.get("distribution", "bcd")
+    trials = request.form.get("trials", "")
+    probability = request.form.get("probability", "")
+    successes = request.form.get("successes", "")
+    return render_template(
+        "binomial-form.html",
+        distribution=distribution,
+        trials=trials,
+        probability=probability,
+        successes=successes,
+    )
 
 
 @app.route("/normal")
